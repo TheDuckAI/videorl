ab6355be1c2ada163dceb972ab3b545dfc7e17b4
t46 <takagi4646@gmail.com>
2023-08-28 23:37:16 +0900

    1fps

--- code/data/streamingdataset/preprocess/video_to_mds.py
+++ code/data/streamingdataset/preprocess/video_to_mds.py
@@ -5,15 +5,14 @@ import numpy as np
 from streaming import MDSWriter
 from tqdm import tqdm
 
-BASE_DIR = "/root/projects/rl-nlp/videos/dataset/00000/"  # NOTE: video2dataset creates more directories other than 00000. I used only 00000 because my machine doesn't have enough space.
+BASE_DIR = "/root/projects/rl-nlp/videos/dataset_downsampled/00000/"  # NOTE: video2dataset creates more directories other than 00000. I used only 00000 because my machine doesn't have enough space.
 
 def convert_time_to_seconds(time_str):
     hours, minutes, seconds = map(float, time_str.split(":"))
     return hours * 3600 + minutes * 60 + seconds
 
-# videos = []
 
-SAVE_PATH = "/root/projects/rl-nlp/videos/data/mds-not-split/00000"
+SAVE_PATH = "/root/projects/rl-nlp/videos/data_downsampled/mds-not-split/00000"
 os.makedirs(SAVE_PATH, exist_ok=True)
 
 COLUMNS = {
@@ -25,7 +24,7 @@ HASHES = 'sha1', 'xxh64'
 
 with MDSWriter(out=SAVE_PATH, columns=COLUMNS, compression=COMPRESSION, hashes=HASHES) as save_file:        
 
-    for video_filename in tqdm(os.listdir(BASE_DIR)[:10]):  # NOTE: I used only 10 videos for the test because the space of my machine is limited.
+    for video_filename in tqdm(os.listdir(BASE_DIR)):  # NOTE: I used only 10 videos for the test because the space of my machine is limited.
         if not video_filename.endswith(".mp4"):
             continue
 
@@ -48,33 +47,36 @@ with MDSWriter(out=SAVE_PATH, columns=COLUMNS, compression=COMPRESSION, hashes=H
         with open(json_file_path, 'r') as f:
             data = json.load(f)
             fps = reader.get_meta_data()['fps']
+
             # fps = data["yt_meta_dict"]["info"]["fps"]  # NOTE: This is not always available
             try:
                 subtitle_data = data["yt_meta_dict"]["subtitles"]  # NOTE: This is not always available
             except KeyError:
                 continue
 
-            for i, frame in enumerate(reader):
+            for subtitle in subtitle_data:
+                start_time = convert_time_to_seconds(subtitle["start"])  # Start time of the subtitle.
+                end_time = convert_time_to_seconds(subtitle["end"])  # End time of the subtitle.
 
-                if frame is None:
-                    break
+                while start_time < end_time:
 
-                current_time = i / fps
+                    frame_idx = int(start_time * fps)
 
-                frame_array = np.array(frame)
-                frames.append(frame_array)
+                    try: 
+                        frame = reader.get_data(frame_idx)
+                    except IndexError:
+                        break  # NOTE: I sometimes get IndexError even when frame_idx is less than the number of frames. I don't know why.
 
-                subtitle_value = np.array([-1])  # NOTE: -1 means no subtitle. if the current time is in the interval of the subtitle, we overwrite this value.
+                    if frame is None:
+                        continue
 
-                for subtitle in subtitle_data:
-                    start_time = convert_time_to_seconds(subtitle["start"])  # Start time of the subtitle.
-                    end_time = convert_time_to_seconds(subtitle["end"])  # End time of the subtitle.
+                    frame_array = np.array(frame)
 
-                    if start_time <= current_time and current_time < end_time:  # NOTE: If the current time is in the interval of the subtitle, we save the subtitle. 
-                        subtitle_value = np.frombuffer(subtitle["lines"][0].encode('utf-8'), dtype=np.uint8)
-                        break
+                    frames.append(frame_array)  # NOTE: Save the frame only when there is a subtitle.
+                    subtitles.append(np.frombuffer(subtitle["lines"][0].encode('utf-8'), dtype=np.uint8))
 
-                subtitles.append(subtitle_value)            
+                    start_time += 1.0  # NOTE: 1fps
+
 
         try:
             MAX_LENGTH = max([len(sub) for sub in subtitles])
@@ -90,4 +92,3 @@ with MDSWriter(out=SAVE_PATH, columns=COLUMNS, compression=COMPRESSION, hashes=H
         }
 
         save_file.write(video_data)
-    # videos.append(video_data)
