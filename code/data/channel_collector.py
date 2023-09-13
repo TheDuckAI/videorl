3ea1d44553dd87ea808642435324ea552ca82ac6
unknown <hu649@purdue.edu>
2023-09-12 17:24:20 -0700

    Use a singular session and adjust parallelism

--- code/data/channel_collector.py
+++ code/data/channel_collector.py
@@ -38,70 +38,64 @@ video_writer = csv_writer(video_file, delimiter = '\t')
 
 
 async def collect_videos(
-    channel_link, channel_writer = channel_writer, video_writer = video_writer
+    channel_link, session, channel_writer = channel_writer, video_writer = video_writer
 ):
-    # use ipv6 (helps with blocks) and leave concurrency to parallel connections
-    conn = TCPConnector(limit = 1, family = socket.AF_INET6, force_close = True)
-    async with ClientSession(
-        base_url = BASE, connector = conn, cookie_jar = DummyCookieJar()
-    ) as session:
-        async with session.get(
-            channel_link + '/videos', headers = {'User-Agent': USER_AGENT}, timeout = 5
-        ) as response:
-            # ignore 404 and just continue
-            if response.status == 404:
-                return
-            
-            if response.ok is False:
-                print('bad response text', await response.text())
-                return response.ok
+    async with session.get(
+        channel_link + '/videos', headers = {'User-Agent': USER_AGENT}, timeout = 10
+    ) as response:
+        # ignore 404 and just continue
+        if response.status == 404:
+            return
         
-            # find the channel data within the html
-            html = await response.text()
-            soup = bs4(html, 'html.parser')
-            data_str = 'var ytInitialData = '
-            channel_data = json.loads(
-                soup(text = re.compile(data_str))[0].strip(data_str).strip(';')
-            )
-            channel_info = [channel_link] + parse_channel_info(channel_data)
-            
-            # ignore title-less or "Topic" channels
-            if channel_info[1] is None or channel_info[1].endswith(" - Topic"):
-                return
-            
-            video_rows, token = parse_videos(channel_data)
+        if response.ok is False:
+            print('bad response: ', response.status, response.reason)
+            return response.ok
+    
+        # find the channel data within the html
+        html = await response.text()
+        soup = bs4(html, 'html.parser')
+        data_str = 'var ytInitialData = '
+        channel_data = json.loads(
+            soup(text = re.compile(data_str))[0].strip(data_str).strip(';')
+        )
+        channel_info = [channel_link] + parse_channel_info(channel_data)
+        
+        # ignore title-less or "Topic" channels
+        if channel_info[1] is None or channel_info[1].endswith(" - Topic"):
+            return
+        
+        video_rows, token = parse_videos(channel_data)
 
-            # some channels may not have videos
-            if video_rows is None:
-                return
+        # some channels may not have videos
+        if video_rows is None:
+            return
 
+        async with write_lock:
+            channel_writer.writerow(channel_info)        
+            video_writer.writerows(video_rows)
+
+    # continue "scrolling" through channel's videos
+    while token is not None:
+        data = copy.deepcopy(PAYLOAD)
+        data['continuation'] = token
+        async with session.post(
+            BROWSE_ENDPOINT, headers = {'User-Agent': USER_AGENT},
+            json = data, timeout = 10
+        ) as response:
+            if response.ok is False:
+                print('bad response: ', response.status, response.reason)
+                return response.ok
+            
+            video_data = await response.json()
+            video_rows, token = parse_videos(video_data, is_continuation = True)
             async with write_lock:
-                channel_writer.writerow(channel_info)        
                 video_writer.writerows(video_rows)
 
-        # continue "scrolling" through channel's videos
-        while token is not None:
-            data = copy.deepcopy(PAYLOAD)
-            data['continuation'] = token
-            async with session.post(
-                BROWSE_ENDPOINT, headers = {'User-Agent': USER_AGENT},
-                json = data, timeout = 5
-            ) as response:
-                if response.ok is False:
-                    print('bad response text', await response.text())
-                    return response.ok
-                
-                video_data = await response.json()
-                video_rows, token = parse_videos(video_data, is_continuation = True)
-                async with write_lock:
-                    video_writer.writerows(video_rows)
 
 
-
-async def worker(channels_left):
-    async with print_lock:
-        print('worker started')
-
+async def worker(channels_left, session):
+    # async with print_lock:
+    #     print('worker started')
     while True:
         async with channel_lock:
             if len(channels_left) == 0:
@@ -110,14 +104,21 @@ async def worker(channels_left):
             channel_link = channels_left.pop()
         
         try:
-             ok_response = await collect_videos(channel_link)
+             ok_response = await collect_videos(channel_link, session)
              if ok_response is False:
                 async with print_lock:
-                    print("bad response, stopping worker (try restarting)")
+                    print("Stopping worker due to bad response (try restarting)")
                 return
+        except asyncio.exceptions.TimeoutError:
+            # retry the channel
+            async with channel_lock:
+                channels_left.append(channel_link)
+            async with print_lock:
+                print('Caught a timeout')
         except Exception as e:
             async with print_lock:
                 print(f'Exception caught for {channel_link}:', e)
+                print(traceback.format_exc())
         
         # async with print_lock:
         #     print('collected all video from the channel', channel_link, end = "\t\t\t\r")
@@ -158,14 +159,20 @@ async def main(num_workers):
                 ['id', 'title', 'date', 'length', 'views']
             )
 
-    # start the workers
-    print('starting workers now, run `wc -l channels.tsv` to monitor number of channels collected')
-    await asyncio.gather(*[
-        worker(channels) for _ in range(num_workers)
-    ])
+    # use a singular session to benefit from connection pooling
+    # use ipv6 (helps with blocks) and leave concurrency to parallel connections
+    conn = TCPConnector(limit = None, family = socket.AF_INET6, force_close = True)
+    async with ClientSession(
+        base_url = BASE, connector = conn, cookie_jar = DummyCookieJar()
+    ) as session:
+        # start the workers
+        print('starting workers now, run `wc -l channels.tsv` to monitor number of channels collected')
+        await asyncio.gather(*[
+            worker(channels, session) for _ in range(num_workers)
+        ])
 
 try:
-    asyncio.run(main(num_workers = 200))
+    asyncio.run(main(num_workers = 50))
 except (KeyboardInterrupt, Exception) as e:
     # print("\nfinal exception:", e)
     print(traceback.format_exc())
